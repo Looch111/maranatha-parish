8952bf956ced7e3049a5853d3a2ec817a52ffb29
Looch Mane <loochmane4@gmail.com>
2025-09-20 19:47:17 +0000

    Try fixing this error: `Runtime Error: Error: No link element found for

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <head>
+      <body className="font-body antialiased h-full flex flex-col relative">
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased h-full flex flex-col relative">
         <Header />
         <main className="flex-1">
           {children}
