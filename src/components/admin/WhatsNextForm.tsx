ff3d3358fb874290fee0aaee193a7ee3cba0baa8
Looch Mane <loochmane4@gmail.com>
2025-09-20 20:35:04 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/admin/WhatsNextForm.tsx
+++ src/components/admin/WhatsNextForm.tsx
@@ -64,7 +64,7 @@ export function WhatsNextForm({ initialData }: { initialData: WhatsNext }) {
            )}
           <SubmitButton />
         </CardFooter>
-      </card>
+      </Card>
     </form>
   );
 }
